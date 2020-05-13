0bd66e0ad8b26942c5e9bdf629f57d82ed8dce48
Felipe Franco <felipe.franco@thomsonreuters.com>
2020-05-13 17:21:57 -0300

    listagem de produtos

--- src/app/cardapio/cardapio-list/cardapio-list.page.ts
+++ src/app/cardapio/cardapio-list/cardapio-list.page.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardapio-list.page.scss'],
 })
 export class CardapioListPage implements OnInit {
+  products: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+
+    for (let i = 0; i < 20; i++) {
+      const product = {
+        category: 'Hambúrguers',
+        name: `Hambúrguer ${i + 1}`,
+        price: (10.5 * (i + 1)),
+        description: 'Pão artesanal de brioche, suculento blend de costela, cheddar cremoso, barbecue da casa, maionese da casa, crispy de bacon, onion rings',
+        photoUrl: 'https://source.unsplash.com/sc5sTPMrVfk/640x531'
+      };
+
+      this.products.push(product);
+    }
   }
 
 }
